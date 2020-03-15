1f678a6153f212d462d35aaf3b250e61273030e4
Nitish Mehta <nitishmehta.nm@gmail.com>
2020-03-16 02:51:51 +0530

    Updates

---
+++ precache-manifest.f59bbca8d66aff93622d6e3255efb660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3932d7fcfca0bf1a213cff177ee5a111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df24b0e65a085ef7b459",
+    "url": "/static/css/main.27fbba52.chunk.css"
+  },
+  {
+    "revision": "b6388aee4fdbde08db39",
+    "url": "/static/js/2.d108d8c7.chunk.js"
+  },
+  {
+    "revision": "8d1d23c112a823e877233124bbc80a59",
+    "url": "/static/js/2.d108d8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df24b0e65a085ef7b459",
+    "url": "/static/js/main.ca33e057.chunk.js"
+  },
+  {
+    "revision": "360ca92e954d2134df9e",
+    "url": "/static/js/runtime-main.ac4d95da.js"
+  }
+]);
