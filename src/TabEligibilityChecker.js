888426fe4bccf23d1596d3def1a3616bc41337a6
Nitish Mehta <nitishmehta.nm@gmail.com>
2020-03-16 19:45:27 +0530

    feat: added state handling for tabEligibility

--- src/TabEligibilityChecker.js
+++ src/TabEligibilityChecker.js
@@ -1,16 +1,38 @@
 import "@ui5/webcomponents/dist/Switch";
-import React from "react";
+import React, { useReducer } from "react";
 
 import "@ui5/webcomponents/dist/Label";
 import "@ui5/webcomponents/dist/Switch";
 
+const initialState = {
+  fever: false,
+  runnyNose: true,
+  soreThroat: false,
+  cough: false,
+  difficultyBreathing: false,
+  relevantTravelHistory: false,
+  relevantContact: false
+};
+
+function reducer(state, action) {
+  console.log(action, state);
+  switch (action.type) {
+    case "toggle":
+      return { ...state, [action.payload]: !state[action.payload] };
+    default:
+      throw new Error();
+  }
+}
+
 const TabEligibilityChecker = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
   return (
     <div>
       <div>Please mark all the symptoms that you are currently facing:</div>
 
-      <div class="eligibilityCheckerForm">
-        <div class="formRow">
+      <div className="eligibilityCheckerForm">
+        <div className="formRow">
           <ui5-label class="formLabel" wrap>
             Fever :
           </ui5-label>
@@ -18,10 +40,12 @@ const TabEligibilityChecker = () => {
             text-on="Yes"
             text-off="No"
             class="formSwitch"
+            checked={state.fever}
+            change={() => dispatch({ type: "toggle", payload: "fever" })}
             graphical
           ></ui5-switch>
         </div>
-        <div class="formRow">
+        <div className="formRow">
           <ui5-label class="formLabel" wrap>
             Runny Nose:
           </ui5-label>
@@ -29,10 +53,12 @@ const TabEligibilityChecker = () => {
             text-on="Yes"
             text-off="No"
             class="formSwitch"
+            checked={state.runnyNose}
+            change={() => dispatch({ type: "toggle", payload: "runnyNose" })}
             graphical
           ></ui5-switch>
         </div>
-        <div class="formRow">
+        <div className="formRow">
           <ui5-label class="formLabel" wrap>
             Sore Throat:
           </ui5-label>
@@ -40,10 +66,12 @@ const TabEligibilityChecker = () => {
             text-on="Yes"
             text-off="No"
             class="formSwitch"
+            checked={state.soreThroat}
+            change={() => dispatch({ type: "toggle", payload: "soreThroat" })}
             graphical
           ></ui5-switch>
         </div>
-        <div class="formRow">
+        <div className="formRow">
           <ui5-label class="formLabel" wrap>
             Cough:
           </ui5-label>
@@ -51,10 +79,12 @@ const TabEligibilityChecker = () => {
             text-on="Yes"
             text-off="No"
             class="formSwitch"
+            checked={state.cough}
+            change={() => dispatch({ type: "toggle", payload: "cough" })}
             graphical
           ></ui5-switch>
         </div>
-        <div class="formRow">
+        <div className="formRow">
           <ui5-label class="formLabel" wrap>
             Difficulty breathing:
           </ui5-label>
@@ -62,11 +92,15 @@ const TabEligibilityChecker = () => {
             text-on="Yes"
             text-off="No"
             class="formSwitch"
+            checked={state.difficultyBreathing}
+            change={() =>
+              dispatch({ type: "toggle", payload: "difficultyBreathing" })
+            }
             graphical
           ></ui5-switch>
         </div>
         <div>Additional Information:</div>
-        <div class="formRow">
+        <div className="formRow">
           <ui5-label class="formLabel" wrap>
             Have you visited any COVID-19 affected countries in last 14 days?
           </ui5-label>
@@ -74,9 +108,13 @@ const TabEligibilityChecker = () => {
             text-on="Yes"
             text-off="No"
             class="formSwitch"
+            checked={state.relevantTravelHistory}
+            change={() =>
+              dispatch({ type: "toggle", payload: "relevantTravelHistory" })
+            }
           ></ui5-switch>
         </div>
-        <div class="formRow">
+        <div className="formRow">
           <ui5-label class="formLabel" wrap>
             Have you been in contact with any confirmed positive case?
           </ui5-label>
@@ -84,12 +122,16 @@ const TabEligibilityChecker = () => {
             text-on="Yes"
             text-off="No"
             class="formSwitch"
+            checked={state.relevantContact}
+            change={() =>
+              dispatch({ type: "toggle", payload: "relevantContact" })
+            }
           ></ui5-switch>
         </div>
       </div>
       <br />
       <br />
-      <ui5-button disabled design="Emphasized" class="eligibilityBtn">
+      <ui5-button design="Emphasized" class="eligibilityBtn">
         What should I do?
       </ui5-button>
     </div>
